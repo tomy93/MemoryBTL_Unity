33c48cbfa6cac6097f7be305ca1a339f53cd7e5c
tomy93 <tomy.tomas.talledo@gmail.com>
2019-03-20 17:32:00 -0500

    Is there an award for this?

---
+++ Assets/_Scripts/WinMessageSetter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinMessageSetter : MonoBehaviour {
+    [SerializeField] private Text lblMessage;
+
+    public void SetMessage(string s) {
+        lblMessage.text = "Lo lograste en " + s + " segundos";
+    }
+}
